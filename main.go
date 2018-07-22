16abbe59c35cdcec6a4cbd7bcb201ed3bb78fba9
Hiruma <yapadubpadoo@gmail.com>
2018-07-22 17:40:54 +0700

    refactor

--- main.go
+++ main.go
@@ -18,78 +18,62 @@ func prettyPrint(s string) {
 	log.Printf("%s", s)
 }
 
-func main() {
+func getRabbit() (*amqp.Connection, *amqp.Channel) {
 	conn, err := amqp.Dial("amqp://tz:tz!@localhost:5630/")
 	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
+	return conn, ch
+}
+
+func setQoS(ch *amqp.Channel) {
+	err := ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
+}
+
+func declareQueue(ch *amqp.Channel, name string) amqp.Queue {
+	q, err := ch.QueueDeclare(
+		name,  // name
+		true,  // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	return q
+}
+
+func consume(ch *amqp.Channel, qName string) <-chan amqp.Delivery {
+	msgs, err := ch.Consume(
+		qName, // queue
+		"",    // consumer
+		false, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,   // args
+	)
+	failOnError(err, "Failed to consume a queue")
+	return msgs
+}
+
+func main() {
+
+	conn, ch := getRabbit()
+	setQoS(ch)
+	defer conn.Close()
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"task_queue", // name
-		true,         // durable
-		false,        // delete when unused
-		false,        // exclusive
-		false,        // no-wait
-		nil,          // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
+	jobQ := declareQueue(ch, "job_queue")
+	settingQ := declareQueue(ch, "setting_queue")
 
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	conn2, err := amqp.Dial("amqp://tz:tz!@localhost:5630/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn2.Close()
-
-	ch2, err := conn2.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch2.Close()
-
-	q2, err := ch2.QueueDeclare(
-		"task_queue2", // name
-		true,          // durable
-		false,         // delete when unused
-		false,         // exclusive
-		false,         // no-wait
-		nil,           // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch2.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
-
-	msgs2, err := ch2.Consume(
-		q2.Name, // queue
-		"",      // consumer
-		false,   // auto-ack
-		false,   // exclusive
-		false,   // no-local
-		false,   // no-wait
-		nil,     // args
-	)
-	failOnError(err, "Failed to register a consumer")
+	msgs := consume(ch, jobQ.Name)
+	msgs2 := consume(ch, settingQ.Name)
 
 	forever := make(chan bool)
 
